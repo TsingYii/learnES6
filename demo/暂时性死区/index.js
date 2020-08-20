e714e6f5523b74bf52a0083d936d4f7aaa75df58
yufang <yufang93@qq.com>
2020-08-20 10:46:08 +0800

    1.暂时性死区  2.为什么需要块级作用域

---
+++ demo/暂时性死区/index.js
@@ -0,0 +1,21 @@
+// var a = 1
+// {
+//     a = 3
+//     let a
+// }
+
+// for (var index = 0; index < 6; index++) {
+//     console.log(index);
+// }
+// console.log(index);
+
+let a = 1
+function add(){
+	console.log(a)
+	if(false){
+		let a = 2
+		console.log(a)
+	}
+}
+
+add()
